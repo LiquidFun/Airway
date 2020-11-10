9b6404fb762208d74eb244552c0fe9373c9414ce
Brutenis Gliwa <brutenis.gliwa@planet-ai.de>
2020-11-10 14:30:37 +0100

    Updated script handle more veins and arteries

--- scripts/seperate-bronchus-files.sh
+++ scripts/seperate-bronchus-files.sh
@@ -3,8 +3,7 @@
 copyImages () {
 
     #getting path to Bronchus-images
-    image_path=`grep -m 1 "$1" $data_file | cut -d ":" -f 3 | sed 's#\\#/#g' | tr -d [:blank:] | tr -d [:cntrl:] | head -n 1`
-    image_path="/$image_path"
+    image_path="/$(grep -m 1 $1 $data_file | grep -oP '(?<=Directory: ).*' | sed 's#\\#/#g' | tr -d [:blank:] | tr -d [:cntrl:] | head -n 1)"
 
     #getting patients id from image path
     pat_id=`echo $image_path | cut -d '/' -f 3`
@@ -19,23 +18,26 @@ copyImages () {
     fi
 
     #copying
-    echo "copying $1 related files from Patient $pat_id"
+    echo -e "Copying $1 related files for patient $pat_id\n\tFrom $src \n\tTo $dest\n"
     cp -r $src $dest
 }
 
-
-path_source='/share/SBI-Projekt/raw_data/'
-path_dest='/share/SBI-Projekt/working_dir/'
+path_source='/home/pirat/Data/raw_data/'
+path_dest='/home/pirat/Data/airway/'
 
 export IFS=$'\n'
 
-declare -a patterns=(   "Bronchus"
-                        "Right upper lobe"
-                        "Right middle lobe"
-                        "Right lower lobe"
-                        "Left upper lobe"
-                        "Left lower lobe"                      
-                        )
+declare -a patterns=(
+    "Bronchus"
+    "Right upper lobe"
+    "Right middle lobe"
+    "Right lower lobe"
+    "Left upper lobe"
+    "Left lower lobe"
+    "Artery"
+    "Vein"
+    "Koerperstamm"
+)
 
 for pat_dir in `ls -1 $path_source`
 do
@@ -45,29 +47,28 @@ do
 
     if [ -e $data_file ]
     then
-
         for pattern in "${patterns[@]}"
         do
-            
             grep "$pattern" $data_file > /dev/null
             if [ $? -ne 0 ]
             then
                 all_images_existing="no"
                 echo "WARNING path $pat_dir has no $pattern related images"
             fi
-
         done
        
         if [ $all_images_existing = "yes" ]
         then
+            copyImages "Koerperstamm"       "Koerperstamm"
             copyImages "Bronchus"           "Bronchus"
             copyImages "Right upper lobe"   "RightUpperLobe"
             copyImages "Right middle lobe"  "RightMiddleLobe"
             copyImages "Right lower lobe"   "RightLowerLobe"
             copyImages "Left upper lobe"    "LeftUpperLobe"
             copyImages "Left lower lobe"    "LeftLowerLobe"
+            copyImages "Artery"             "Artery"
+            copyImages "Vein"               "Vein"
         fi
-
     fi
 done
 
