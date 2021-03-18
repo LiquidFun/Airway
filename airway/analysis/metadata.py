558aad5bc8f31bb2663f6b6d17a4f5a0be6ae9a0
Brutenis Gliwa <brutenis.gliwa@planet-ai.de>
2021-03-18 15:23:08 +0100

    Updated metadata script

--- airway/analysis/metadata.py
+++ airway/analysis/metadata.py
@@ -4,22 +4,24 @@ import csv
 from airway.util.util import get_data_paths_from_args
 from airway.util.util import get_patient_name
 
+keys_of_interest = [
+    "PatientID",
+    "AdditionalPatientHistory",
+    "PatientSex",
+    "AcquisitionDate",
+    "Manufacturer",
+    "ManufacturerModelName",
+    "SliceThickness",
+]
+
 
 def read_metadata(path):
     patients = []
-    for patient in path.glob("*"):
-        data = pydicom.dcmread(path / patient / "Bronchus" / "IMG1")
+    for patient_id in path.glob("*"):
+        data = pydicom.dcmread(path / patient_id / "Bronchus" / "IMG1")
         values = {}
-        keys = ["PatientID",
-                "AdditionalPatientHistory",
-                "PatientSex",
-                "AcquisitionDate",
-                "Manufacturer",
-                "ManufacturerModelName",
-                "SliceThickness"
-                ]
-        for key in keys:
-            values[key] = str(data.data_element(key).value)
+        for key in keys_of_interest:
+            values[key] = str(data.data_element(key).value) if key in data else ""
         values["Name"] = get_patient_name(values["PatientID"])
         patients.append(values)
     return patients
@@ -33,38 +35,25 @@ def write_table(path, data):
 
 
 def reformat_data(data):
-    d = sorted(data, key=lambda dic: int(dic["PatientID"]))
-    for i in range(len(d)):
-        d[i]["#"] = str(i + 1)
-    new_data = [["#",
-                 "Name",
-                 "PatientID",
-                 "PatientSex",
-                 "AdditionalPatientHistory",
-                 "AcquisitionDate",
-                 "Manufacturer",
-                 "ManufacturerModelName",
-                 "SliceThickness"
-                 ]]
-    for patient in d:
-        line = []
-        for key in new_data[0]:
-            line.append(patient[key])
-        new_data.append(line)
-    return new_data
+    sorted_data = sorted(data, key=lambda dic: int(dic["PatientID"]))
+    table = [["#", "Name", *keys_of_interest]]
+    # Add new entry list for every patient where the table is used as reference
+    for index, patient in enumerate(sorted_data, 1):
+        patient["#"] = str(index)
+        table.append([patient[key] for key in table[0]])
+    return table
 
 
 def main():
     output_data_path, input_data_path = get_data_paths_from_args()
     data = read_metadata(input_data_path)
-    formatted_data = reformat_data(data)
-    first_row = True
-    for patient in formatted_data:
-        print('| ', ' | '.join(patient), ' |', sep='')
-        if first_row:
-            print("|:----" * len(patient), end='|\n')
-            first_row = False
-    write_table(output_data_path, formatted_data)
+    formatted_table = reformat_data(data)
+    for formatted_line in formatted_table:
+        print('| ', ' | '.join(formatted_line), ' |', sep='')
+        # If is first line print formatting for markdown
+        if formatted_line[0] == '#':
+            print("|:----" * len(formatted_line), end='|\n')
+    write_table(output_data_path, formatted_table)
 
 
 if __name__ == "__main__":
