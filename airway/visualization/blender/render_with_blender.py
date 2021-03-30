e2aa3245ca034be4b686c3846fd4b8226b6706e8
Brutenis Gliwa <brutenis.gliwa@planet-ai.de>
2021-03-30 17:48:49 +0200

    Added camera lens depth, now its the same as before

--- airway/visualization/blender/render_with_blender.py
+++ airway/visualization/blender/render_with_blender.py
@@ -104,6 +104,7 @@ def rad(degrees):
 camera = bpy.data.objects['Camera']
 camera.location = (0, 16, 0)
 camera.rotation_euler = (rad(90), 0, rad(-180))
+camera.lens = 35
 hide(camera, viewport=True)
 
 # Set rendering options
